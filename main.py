402410fea2c52e27c4a0f0a483c42842e57b99de
drewfullerton <drewdfullerton@gmail.com>
2026-01-07 20:54:01 -0600

    Swapped player for groups to make updates and drawing easier

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1 > 0:
         for event in pygame.event.get():
@@ -18,8 +21,9 @@ def main():
                 return
         log_state()
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = Clock.tick(60)/ 1000
 
